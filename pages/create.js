3a9fed701f558a82245753236cca3146dc6d2450
Kewey <jordan-souchez@hotmail.fr>
2021-04-01 13:10:17 +0200

    add auth

---
+++ pages/create.js
@@ -0,0 +1,79 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Form.module.css'
+import { useForm } from 'react-hook-form'
+
+export default function Create() {
+	const { register, handleSubmit } = useForm()
+	let loading = false
+	const onSubmit = (data) => {
+		console.log(`data`, data)
+		loading = true
+		fetch('http://127.0.0.1/api/users', { method: 'POST', body: data })
+			.then(() => console.log('logged'))
+			.catch((err) => console.log('error', err))
+			.finally(() => (loading = false))
+	}
+
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Inscription | Vroom vroom</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className={styles.main}>
+				<h1 className={styles.title}>Inscrivez vous</h1>
+
+				<div className={styles.grid}>
+					<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+						<div className={styles.input}>
+							<label htmlFor='firstName'>Prénom</label>
+							<input
+								name='firstName'
+								id='firstName'
+								type='text'
+								ref={register({ required: true })}
+							/>
+						</div>
+						<div className={styles.input}>
+							<label htmlFor='lastName'>Nom</label>
+							<input
+								name='lastName'
+								id='lastName'
+								type='text'
+								ref={register({ required: true })}
+							/>
+						</div>
+						<div className={styles.input}>
+							<label htmlFor='email'>E-mail</label>
+							<input
+								name='email'
+								id='email'
+								type='text'
+								ref={register({ required: true })}
+							/>
+						</div>
+						<div className={styles.input}>
+							<label htmlFor='password'>Mot de passe</label>
+							<input
+								name='password'
+								id='password'
+								type='password'
+								ref={register({ required: true })}
+							/>
+						</div>
+						<button disabled={loading} type='submit'>
+							{loading ? 'Chargement' : 'Inscription'}
+						</button>
+					</form>
+					<Link href='/login'>Déjà un compte ? Connectez vous</Link>
+				</div>
+			</main>
+
+			<footer className={styles.footer}>
+				<p>Powered by Tom & Jordan</p>
+			</footer>
+		</div>
+	)
+}
---
+++ pages/login.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Form.module.css'
+
+export default function Login() {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Connexion | Vroom vroom</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className={styles.main}>
+				<h1 className={styles.title}>Connectez vous</h1>
+
+				<div className={styles.grid}>
+					<form className={styles.form}>
+						<div className={styles.input}>
+							<label htmlFor='email'>E-mail</label>
+							<input name='email' id='email' type='text' />
+						</div>
+						<div className={styles.input}>
+							<label htmlFor='password'>Mot de passe</label>
+							<input name='password' id='password' type='password' />
+						</div>
+						<button>Connexion</button>
+					</form>
+				</div>
+				<Link href='/create'>Pas de compte ? Inscrivez vous</Link>
+			</main>
+
+			<footer className={styles.footer}>
+				<p>Powered by Tom & Jordan</p>
+			</footer>
+		</div>
+	)
+}
